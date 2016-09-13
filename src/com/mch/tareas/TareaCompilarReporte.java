a630242f54a79d9023f339f4c091c86367634fbe
Juan <jgerena@sistematizando.com>
2016-09-13 14:21:35 -0500

    se sube ajuste de factura san rafael

---
+++ src/com/mch/tareas/TareaCompilarReporte.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.mch.tareas;
+
+
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+
+import com.mch.utilidades.UtilMCH;
+
+/**
+ * @author Juan Camilo
+ *
+ */
+public class TareaCompilarReporte {
+
+
+	public static void main(String[] args) {
+		TareaCompilarReporte compilar = new TareaCompilarReporte();
+		String reporte = "facturaSanRafael";
+		System.out.println(compilar.compilar(reporte));
+		
+
+	}
+
+	
+	public String compilar(String reporte){
+		String respuesta ="";
+		
+		String ruta = UtilMCH.getRutaProyecto().replace("bin/", "");
+		
+		String origen = ruta+"reportes/"+reporte+"/"+reporte+".jrxml";
+		String destino = ruta+"reportes/"+reporte+"/"+reporte+".jasper";
+		
+		System.out.println(origen+"\n"+destino);
+		
+		  try{ 
+		      JasperCompileManager.compileReportToFile(origen,destino);
+		      respuesta = "Reporte Compilado con exito";
+		    }     
+		    catch (JRException e) {
+		    	System.out.println("error"+e.getMessage());    	
+		    	respuesta = "No se pudo compilar el reporte (" + reporte + ") ERROR--> " + e.getMessage();
+		      throw new RuntimeException("No se pudo compilar el reporte (" + reporte + ") ERROR--> " + e.getMessage());
+		    }
+		
+		return respuesta;
+	}
+	
+}
